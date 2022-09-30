9787e56cc6efe1cb148e28e23afbaf7f9f946e90
kh4fin <113575348+kh4fin@users.noreply.github.com>
2022-10-01 06:20:43 +0700

    Add files via upload

---
+++ GANJIL GENAP-JAVA/bilGenap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// nama : muhammad abdul khafid
+//squad : 3
+
+using namespace std;
+
+int main(){
+	
+//  mencari bilangan genap	
+	int a =0;
+	
+	while(a < 20){
+		
+			a+=2;
+		cout << "bilangan ke - ";
+		cout << a << endl;
+	}
+
+	
+	return 0;
+}
