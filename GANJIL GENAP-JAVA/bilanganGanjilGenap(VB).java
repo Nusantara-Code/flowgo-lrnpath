cc09dcb3b6b6118018d41ae3945d28af7fef8ebd
Affandyy <113109420+VannByte@users.noreply.github.com>
2022-10-02 23:15:11 +0700

    Add files via upload

---
+++ GANJIL GENAP-JAVA/bilanganGanjilGenap(VB).java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+	public static void main(String[] args) {
+		int bilangan;
+
+		bilangan = 0;
+		while (bilangan < 20) {
+			bilangan = bilangan + 2;
+			System.out.println("bilangan ke-" + bilangan);
+		}
+	}
+}
