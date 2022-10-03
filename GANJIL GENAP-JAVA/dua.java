b76bacdddffa470fc7e012cc081c63a00c8f50f2
Erwin Setiawan <112855308+0xErs@users.noreply.github.com>
2022-10-03 14:41:03 +0700

    Add files via upload

---
+++ GANJIL GENAP-JAVA/dua.java
@@ -0,0 +1,16 @@
+import java.util.Scanner ;
+public class dua {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in) ;
+
+        int angka ;
+        angka = 0 ;
+        while (angka<20) {
+            angka=angka+2 ;
+            System.out.println(angka);
+        }
+
+    }
+    
+}
