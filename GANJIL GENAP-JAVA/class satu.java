d84cc5f1b6b57b4d1b53c198270563e7d88a7097
awroni23 <113541434+awroni23@users.noreply.github.com>
2022-10-03 19:51:43 +0700

    Add files via upload

---
+++ GANJIL GENAP-JAVA/class satu.java
@@ -0,0 +1,16 @@
+//Roni syahputra
+
+class satu {
+    public static void main(String[] args) {
+        int x;
+
+        x = 0;
+        while (x <= 19) {
+            x = x+2;
+            System.out.println("looping ke=" + x);
+            System.out.println();
+
+        }
+    }
+    
+}
